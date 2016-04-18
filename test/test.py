ccdf49cd37ba31ce092a0d81150855754ed7615c
ski2per <ski2per@163.com>
2016-04-18 16:48:46 +0800

    Add test folder

---
+++ test/test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+# import nltk
+# import jieba.posseg
+
+
+saying = ['After', 'all', 'is', 'said', 'and', 'done', 'more', 'is', 'said', 'than', 'done']
+tokens = set(saying)
+print tokens
+tokens = sorted(tokens)
+print tokens
+print tokens[-2:]
