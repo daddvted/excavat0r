b8f2b9d0fbf834e41d880ed5a863872b90a0da6e
ski2per <ski2per@163.com>
2016-06-22 17:08:36 +0800

    Finish AirQuality.py

--- engine/PPRFTHandler.py
+++ engine/PPRFTHandler.py
@@ -1,6 +1,22 @@
 # P(Press)P(Publication)R(Radio)F(Film)T(Television)
+import requests
+from bs4 import BeautifulSoup
 
-import tornado.web
+
+
+def crawl_info_to_db():
+    config = {
+        'user': 'root',
+        'password': 'hello',
+        'host': '192.168.1.91',
+        'port': '3306',
+        'database': 'excavator',
+        'raise_on_warnings': True,
+    }
+
+    conn = mysql.connector.connect(**config)
+    cursor = conn.cursor()
+    pass
 
 
 
