36d752094da845d208376bfec96e5af9659b2e52
ski2per <ski2per@163.com>
2016-07-06 00:14:47 +0800

    Add EnvProtectionStdSpider.py

---
+++ spider/EnvProtectionStdSpider.py
@@ -0,0 +1,78 @@
+"""
+环境保护部 - 国家环境保护标准
+URL: http://datacenter.mep.gov.cn/trs/query.action
+"""
+import time
+import random
+import requests
+import lxml.html
+import mysql.connector
+
+
+class EnvProtectionStdSpider(object):
+    config = {
+        'user': 'root',
+        'password': 'hello',
+        'host': '192.168.86.86',
+        'port': '3306',
+        'database': 'service',
+        'raise_on_warnings': True,
+
+    }
+    url = "http://datacenter.mep.gov.cn/trs/query.action"
+
+    def __init__(self):
+        self.conn = mysql.connector.connect(**self.config)
+        self.cursor = self.conn.cursor()
+
+        self.form_data = {
+            "docsource": "all"
+        }
+
+    def save2db(self, data):
+        # template = "INSERT INTO env_protection_std(std_no, std_name, std_name_en, std_release_time, " \
+        #         "std_impl_time, std_digest, std_link) VALUES ('{std_no}', '{std_name}', '{std_name_en}'," \
+        #         " '{std_release_time}', '{std_impl_time}', '{std_digest}', '{std_link}')"
+
+        template = ("INSERT INTO env_protection_std "
+                    "(std_no, std_name, std_name_en, std_release_time, std_impl_time, std_digest, std_link) "
+                    "VALUES (%(std_no)s, %(std_name)s, %(std_name_en)s, %(std_release_time)s, %(std_impl_time)s, %(std_digest)s, %(std_link)s)")
+        # sql = template.format(**data)
+        self.cursor.execute(template, data)
+        self.conn.commit()
+
+    def crawl(self):
+        for m in range(2, 45):
+            print("====== Processing page {0} ======".format(m))
+            self.form_data["page.pageNo"] = m
+            browser = requests.post(self.url, data=self.form_data)
+
+            if browser.status_code == 200:
+                root = lxml.html.fromstring(browser.text)
+                divs = root.xpath('//*[@style="display: none;"]')
+
+                for div in divs:
+                    trs = div.xpath('.//tr')
+                    data = {
+                        "std_no": trs[0].xpath('./td[2]')[0].text_content().strip(),
+                        "std_name": trs[1].xpath('./td[2]')[0].text_content().strip(),
+                        "std_name_en": trs[2].xpath('./td[2]')[0].text_content().strip(),
+                        "std_release_time": trs[3].xpath('./td[2]')[0].text_content().strip(),
+                        "std_impl_time": trs[4].xpath('./td[2]')[0].text_content().strip(),
+                        "std_digest": trs[5].xpath('./td[2]')[0].text_content().strip(),
+                        "std_link": trs[6].xpath('./td[2]/a')[0].attrib["href"],
+                    }
+                    self.save2db(data)
+            else:
+                print(browser.status_code)
+                print("Error when crawling page {0}".format(m))
+
+            time.sleep(random.randint(2, 6))
+
+
+if __name__ == "__main__":
+    spider = EnvProtectionStdSpider()
+    spider.crawl()
+
+    spider.cursor.close()
+    spider.conn.close()
--- test.py
+++ test.py
@@ -1,74 +1,14 @@
-import lxml.html
 
-html = """
-<html>
-<head>
-    <title>test</title>
-</head>
-<body>
-    <div id="nothing">
-        <table>
-            <tr>
-                <td>1</td><td>Game of Thrones</td>
-            </tr>
-        </table>
-    </div>
+data = {
+    "name": "usa's apple",
+    "price": 3.5
+}
 
-    <p>hello</p>
-    <div id="content">
-        <table id="fruit">
-            <tr>
-                <td>1</td><td>apple</td>
-            </tr>
-            <tr>
-                <td>2</td><td>pear</td>
-            </tr>
-        </table>
+template = "INSERT INTO fruit(name, price) VALUES({name}, {price})"
 
-        <table id="os">
-            <tr>
-                <td>1</td><td>Linux</td>
-            </tr>
-            <tr>
-                <td>2</td><td>OS X</td>
-            </tr>
-            <tr>
-                <td>3</td><td>Windows</td>
-            </tr>
-        </table>
-
-        <table id="car">
-            <tr>
-                <td>1</td><td>Golf R</td>
-            </tr>
-            <tr>
-                <td>2</td><td>Focus RS</td>
-            </tr>
-            <tr>
-                <td>3</td><td>A AMG</td>
-            </tr>
-            <tr>
-                <td>4</td><td>Audi RS3</td>
-        </tr>
-    </table>
-    </div>
-</body>
-</html>
-"""
+a = ("hello"
+     "world")
+print(type(a))
+print(len(a))
 
 
-root = lxml.html.fromstring(html)
-
-body = root.xpath('/html/body')
-tables = body[0].xpath('//table')
-print(len(tables))
-
-# tables[0] is first table
-trs1 = tables[0].xpath('.//tr')
-trs2 = tables[0].xpath('//tr')
-# same as line above
-trs3 = root.xpath('//tr')
-
-print(len(trs1))
-print(len(trs2))
-print(len(trs3))
