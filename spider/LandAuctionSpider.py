0c72e1fee53f435df66324564e147fe2a6affa27
ski2per <ski2per@163.com>
2016-08-19 14:11:29 +0800

    update

--- spider/LandAuctionSpider.py
+++ spider/LandAuctionSpider.py
@@ -23,6 +23,7 @@ class LandAuctionSpider(Larva):
 
     }
     base_url = "http://www.cdggzy.com:8112"
+    post_url = "http://www.cdggzy.com:8112/two/pmjg_Detail.aspx"
 
     def __init__(self):
         # Init mysql
@@ -43,16 +44,14 @@ class LandAuctionSpider(Larva):
 
     def save2db(self, data):
 
-        template = "INSERT INTO sportgoods(name, principal, tel, addr, website, area, class, latitude, longitude) "\
-                   "VALUES (%(name)s, %(principal)s, %(tel)s, %(addr)s, %(website)s, %(area)s, %(class)s, %(latitude)s, %(longitude)s)"
-        # template = "INSERT INTO fitnesspath(name, principal, tel, addr, website, area, latitude, longitude) "\
-        #            "VALUES (%(name)s, %(principal)s, %(tel)s, %(addr)s, %(website)s, %(area)s, %(latitude)s, %(longitude)s)"
+        template = "INSERT INTO landauction(no, addr, area, price, winner, date) "\
+                   "VALUES (%(no)s, %(addr)s, %(area)s, %(price)s, %(winner)s, %(date)s)"
         self.cursor.execute(template, data)
         self.conn.commit()
 
     def prepare2crawl(self):
-        url = "http://www.cdggzy.com:8112/two/pmjg_Detail.aspx"
-        for p in range(1, 2):
+        for p in range(3, 13):
+            print("----------- Crawling page {} -----------".format(p))
 
             data = {
                 '__VIEWSTATE': '/wEPDwULLTExNzkxNTY4MjEPZBYCAgMPZBYEAgEPFgIeC18hSXRlbUNvdW50Ag8WHmYPZBYCZg8VAwoyMDE2LzA4LzE5BjMwNjU1NTHmi43ljZbkvJrmiJDkuqTnu5PmnpzkuIDop4jooagoMjAxNuW5tDA45pyIMTnml6UpZAIBD2QWAmYPFQMKMjAxNi8wOC8wOQYyOTEyODAx5ouN5Y2W5Lya5oiQ5Lqk57uT5p6c5LiA6KeI6KGoKDIwMTblubQwOOaciDA55pelKWQCAg9kFgJmDxUDCjIwMTYvMDgvMDQGMjkwNDQ5MeaLjeWNluS8muaIkOS6pOe7k+aenOS4gOiniOihqCgyMDE25bm0MDjmnIgwNOaXpSlkAgMPZBYCZg8VAwoyMDE2LzA3LzI3BjI4MjkyMDHmi43ljZbkvJrmiJDkuqTnu5PmnpzkuIDop4jooagoMjAxNuW5tDA35pyIMjfml6UpZAIED2QWAmYPFQMKMjAxNi8wNy8yMQYyNzY3OTAx5ouN5Y2W5Lya5oiQ5Lqk57uT5p6c5LiA6KeI6KGoKDIwMTblubQwN+aciDIx5pelKWQCBQ9kFgJmDxUDCjIwMTYvMDcvMTMGMjcwMjMwMeaLjeWNluS8muaIkOS6pOe7k+aenOS4gOiniOihqCgyMDE25bm0MDfmnIgxM+aXpSlkAgYPZBYCZg8VAwoyMDE2LzA3LzA2BjI2NjY1MzHmi43ljZbkvJrmiJDkuqTnu5PmnpzkuIDop4jooagoMjAxNuW5tDA35pyIMDbml6UpZAIHD2QWAmYPFQMKMjAxNi8wNi8yOQYyNTkxMTMx5ouN5Y2W5Lya5oiQ5Lqk57uT5p6c5LiA6KeI6KGoKDIwMTblubQwNuaciDI55pelKWQCCA9kFgJmDxUDCjIwMTYvMDYvMjEGMjUyNzEzMeaLjeWNluS8muaIkOS6pOe7k+aenOS4gOiniOihqCgyMDE25bm0MDbmnIgyMeaXpSlkAgkPZBYCZg8VAwoyMDE2LzA2LzE2BjI0ODE5OTHmi43ljZbkvJrmiJDkuqTnu5PmnpzkuIDop4jooagoMjAxNuW5tDA25pyIMTbml6UpZAIKD2QWAmYPFQMKMjAxNi8wNi8xNAYyNDM1MjEx5ouN5Y2W5Lya5oiQ5Lqk57uT5p6c5LiA6KeI6KGoKDIwMTblubQwNuaciDE05pelKWQCCw9kFgJmDxUDCjIwMTYvMDYvMDEGMjM2MTQxMeaLjeWNluS8muaIkOS6pOe7k+aenOS4gOiniOihqCgyMDE25bm0MDbmnIgwMeaXpSlkAgwPZBYCZg8VAwoyMDE2LzA1LzMxBjIzNTM0NTHmi43ljZbkvJrmiJDkuqTnu5PmnpzkuIDop4jooagoMjAxNuW5tDA15pyIMzHml6UpZAIND2QWAmYPFQMKMjAxNi8wNS8yNQYyMzM2MjUx5ouN5Y2W5Lya5oiQ5Lqk57uT5p6c5LiA6KeI6KGoKDIwMTblubQwNeaciDI15pelKWQCDg9kFgJmDxUDCjIwMTYvMDUvMTkGMjMwNTM1MeaLjeWNluS8muaIkOS6pOe7k+aenOS4gOiniOihqCgyMDE25bm0MDXmnIgxOeaXpSlkAgMPDxYEHgtSZWNvcmRjb3VudAKpAR4QQ3VycmVudFBhZ2VJbmRleAIBZGRknBpMzCh1lVAb0hQ+KYqaC3XY/ObAUBQzQyX+ubYfCdU=',
@@ -60,31 +59,45 @@ class LandAuctionSpider(Larva):
                 '__EVENTARGUMENT': 1,
             }
             self.headers["User-Agent"] = random.choice(self.USER_AGENTS)
-            browser = requests.post(url, headers=self.headers, data=urlencode(data))
+            browser = requests.post(self.post_url, headers=self.headers, data=urlencode(data))
             if browser.status_code == 200:
-                root = lxml.html.fromstring(browser.text)
-                lis = root.xpath('//div[@class="list1"]/li')
+                html = lxml.html.fromstring(browser.text)
+                lis = html.xpath('//div[@class="list1"]/li')
                 for li in lis:
-                    date = li.xpath('//span')[0].text_content().strip()
-                    date = date.replace('/', '-')
+                    href = li.xpath('./a')[0].attrib["href"]
+                    url = href.replace('..', '')
 
-                    url = li.xpath('//a')[0].attrib["href"]
-                    url = url.replace('..', '')
-
-                    self.crawl(date, self.base_url + url)
-
+                    self.crawl(self.base_url + url)
             else:
                 print("Error crawling page {}".format(p))
 
-    def crawl(self, date, url):
-        # for _ in range(1, 2):
+    def crawl(self, url):
+        self.headers["User-Agent"] = random.choice(self.USER_AGENTS)
+        browser = requests.get(url, headers=self.headers)
+        if browser.status_code == 200:
+            html = lxml.html.fromstring(browser.text)
+            src = html.xpath('//iframe[@id="wzzwInfo"]')[0].attrib["src"]
+            url = self.base_url + src.replace('..', '')
+            print(url)
+            m = re.search(r'\d\d\d\d-\d\d-\d\d', url)
+            date = m.group(0)
 
-            self.headers["User-Agent"] = random.choice(self.USER_AGENTS)
+
             browser = requests.get(url, headers=self.headers)
-            if browser.status_code == 200:
-                print(browser.text)
-
-
+            html = lxml.html.fromstring(browser.text)
+            trs = html.xpath('//table/tr')
+            for tr in trs:
+                tds = tr.xpath('.//td')
+                if len(tds):
+                    data = {
+                        "no": tds[1].text_content().strip(),
+                        "addr": tds[2].text_content().strip(),
+                        "area": tds[3].text_content().strip(),
+                        "price": tds[4].text_content().strip(),
+                        "winner": tds[5].text_content().strip(),
+                        "date": date,
+                    }
+                    self.save2db(data)
 
 if __name__ == "__main__":
     spider = LandAuctionSpider()
